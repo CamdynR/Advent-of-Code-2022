c71f1cf64592a0dac3c238c919e64cc856bd080f
Camdyn Rasque <rasque.camdyn@gmail.com>
2022-12-05 21:55:57 -0800

    Finished day 6

---
+++ day-06/challenge-2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let input = fs.readFileSync(__dirname + '/input-real.txt', {
+  encoding: 'UTF-8',
+});
+
+let windowLength = 14;
+
+for (let i = 0; i < input.length - windowLength; i++) {
+  let marker = input.substring(i,i+windowLength);
+  if (new Set(marker.split('')).size == windowLength) {
+    console.log(`Marker location: ${i+windowLength}`);
+    return;
+  }
+}
